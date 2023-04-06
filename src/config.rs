008040f558f70fbc274d6073d82944397432ae15
SET001 <settydark@gmail.com>
2023-04-07 01:48:46 +0300

    up

--- src/config.rs
+++ src/config.rs
@@ -15,11 +15,7 @@ impl Default for GameConfig {
     GameConfig {
       title: "Bevy potions side effect".into(),
       tile_size: UVec2::new(16, 16),
-<<<<<<< HEAD
-      map_size: UVec2::new(40, 40),
-=======
       map_size: UVec2::new(25, 25),
->>>>>>> master
       gravity: -4.,
       move_speed: 200.,
       scale: 2.,
